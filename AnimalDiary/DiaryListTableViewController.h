7e799a5353323356996977eb88a4d652343a3d20
younghwani <younghwankim624@gmail.com>
2022-11-28 23:50:41 +0900

    second commit

---
+++ AnimalDiary/DiaryListTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  DiaryListTableViewController.h
+//  AnimalDiary
+//
+//  Created by kimyounghwan on 2022/11/28.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DiaryListTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
