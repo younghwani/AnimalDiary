8293d8e8d16daa948a22d8dc2be56ec305ee3d5a
younghwani <younghwankim624@gmail.com>
2022-11-28 23:43:44 +0900

    initial commit

---
+++ AnimalDiary/Model/Diary.h
@@ -0,0 +1,23 @@
+//
+//  Diary.h
+//  AnimalDiary
+//
+//  Created by kimyounghwan on 2022/11/28.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Diary : NSObject
+
+@property (strong, nonatomic) NSString* content;
+@property (strong, nonatomic) NSDate* insertDate;
+
+- (instancetype)initWithContent:(NSString*)content;
+
++ (NSArray*)dummyDiaryList;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AnimalDiary/Model/Diary.m
@@ -0,0 +1,28 @@
+//
+//  Diary.m
+//  AnimalDiary
+//
+//  Created by kimyounghwan on 2022/11/28.
+//
+
+#import "Diary.h"
+
+@implementation Diary
+
+- (instancetype)initWithContent:(NSString*)content {
+    self = [super init];
+    if (self != nil) {
+        _content = content;
+    }
+    return self;
+}
+
++ (NSArray*)dummyDiaryList {
+    Diary* diary1 = [[Diary alloc] initWithContent:@"말말말말말"];
+    Diary* diary2 = [[Diary alloc] initWithContent:@"소소소소소"];
+    Diary* diary3 = [[Diary alloc] initWithContent:@"개개개개개"];
+    
+    return [NSArray arrayWithObjects:diary1, diary2, diary3, nil];
+}
+
+@end
